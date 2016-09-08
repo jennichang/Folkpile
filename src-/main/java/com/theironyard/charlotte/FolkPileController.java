8a26ad830d6d02c7133895a57b2d3c5f51a63d59
Meekins Works <meekinsworks@outlook.com>
2016-09-08 19:18:44 -0400

    People, Group classes, controllers, and Repositories postedd. Get, Post, delete, and put methods included -Joy

--- src-/main/java/com/theironyard/charlotte/FolkPileController.java
+++ src-/main/java/com/theironyard/charlotte/FolkPileController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 
 public class FolkPileController {
-
+// Waiting to import til get configuration clarity or fix 
     @Autowired
     PeopleRepository persons;
 
