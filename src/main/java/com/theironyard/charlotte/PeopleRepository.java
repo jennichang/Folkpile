2a1576f77828b971bdd6cedf4c57954024bf7a2e
Jennifer Chang <jennichang02@gmail.com>
2016-09-09 13:47:06 -0400

    Tweaking query

--- src/main/java/com/theironyard/charlotte/PeopleRepository.java
+++ src/main/java/com/theironyard/charlotte/PeopleRepository.java
@@ -10,7 +10,7 @@ public interface PeopleRepository extends CrudRepository<Person, Integer> {
     //List<Person> findAllByName(String name);
 
 
-    @Query("SELECT p FROM people p WHERE p.first_name LIKE %?1%")
+    @Query("SELECT p FROM person p WHERE p.first_name LIKE %?1%")
     List<Person> findByNameStartsWith(String firstName); //can have customized queries.
 
     //use containing in JPA, won't be a custom query
