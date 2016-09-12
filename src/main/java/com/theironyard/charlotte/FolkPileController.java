4b63cf568b719dd546c54cd3aeb4e793ea663003
Jennifer Chang <jennichang02@gmail.com>
2016-09-12 10:52:36 -0400

    ugh

--- src/main/java/com/theironyard/charlotte/FolkPileController.java
+++ src/main/java/com/theironyard/charlotte/FolkPileController.java
@@ -82,7 +82,7 @@ public class FolkPileController {
     //Add person to group -- and group to person
     @CrossOrigin
     @RequestMapping(path = "/group/{id}", method = RequestMethod.PUT)
-    public void addPersonToGroup(@PathVariable("id") int id, @RequestBody int personId) {
+    public void addPersonToGroup(@PathVariable("id") int id, @RequestBody Person person) {
 
         //Original based on Ben's
 //        Person p = person; //this is the person object to be updated
@@ -96,10 +96,10 @@ public class FolkPileController {
         //Try1:
         //what i originally had
                 Group g = groups.findOne(id);
-                g.people.add(people.findOne(personId));
+                g.people.add(person);
                 groups.save(g); //think this adds a person to the group
 
-                Person p = people.findOne(personId);
+                Person p = people.findOne(person.getId());
                 p.groups.add(groups.findOne(id));
                 people.save(p); //have to update the people table too though right?
 
